80f04cea254146ac9785175448715bedf4f73350
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2021-11-20 12:45:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+canvas = new fabric.Canvas("myCanvas");
+ball_x = 0;
+ball_y = 0;
+hole_y = 400;
+hole_x = 800;
+block_image_width = 5;
+block_image_height = 5;
+golf_obj = "";
+ball_obj = "";
+
+function load_img() {
+	fabric.Image.fromURL("golf-h.png", function (Img) {
+		golf_obj = Img;
+		golf_obj.scaleToWidth(50);
+		golf_obj.scaleToHeight(50);
+		golf_obj.set({
+			top: hole_y,
+			left: hole_x
+		});
+		canvas.add(golf_obj);
+	});
+	new_image();
+}
+
+function new_image() {
+	fabric.Image.fromURL("ball.png", function (Img1) {
+		ball_obj = Img1;
+		ball_obj.scaleToWidth(50);
+		ball_obj.scaleToHeight(50);
+		ball_obj.set({
+			top: ball_y,
+			left: ball_x
+		});
+		canvas.add(ball_obj);
+	});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	if (ball_x == hole_x && ball_y == hole_y) {
+		canvas.remove(ball_obj);
+		document.getElementById("myCanvas").style.borderColor = "red";
+		document.getElementById("hd3").innerHTML = "You Have Hit The Goal!!!"
+	} else {
+		if (keyPressed == '38') {
+			up();
+			console.log("up");
+		}
+		if (keyPressed == '40') {
+			down();
+			console.log("down");
+		}
+		if (keyPressed == '37') {
+			left();
+			console.log("left");
+		}
+		if (keyPressed == '39') {
+			right();
+			console.log("right");
+		}
+	}
+
+	function up() {
+		if (ball_y >= 0) {
+			ball_y = ball_y - block_image_width;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function down() {
+		if (ball_y <= 450) {
+			ball_y = ball_y + block_image_height;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function left() {
+		if (ball_x >= 0) {
+			ball_x = ball_x - block_image_height;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function right() {
+		if (ball_x <= 1050) {
+			ball_x = ball_x + block_image_height;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+}
